7e3919881cdd0e7501175381108a0e6bf3652fe6
Lidia Paduraru <lidia.paduraru@gmail.com>
2018-03-13 15:07:07 +0200

    Filter toggle checkbox list

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
 
 
-  var sidebarTrigger = $('.cd-nav-trigger');
+  var sidebarTrigger = $('#main_menu_toggle');
   sidebarTrigger.on('click', function(event){
     event.preventDefault();
     if(sidebarTrigger.hasClass('nav-is-visible')){
@@ -12,8 +12,19 @@ jQuery(document).ready(function(){
       $('#main-overlay').show();
     }
     $([sidebarTrigger]).toggleClass('nav-is-visible');
-    openNav();
   });
+
+  $('#filter_button').on('click', function(event){
+    event.preventDefault();
+    $('#filter_menu').addClass('visible');
+    $('#main-overlay').show();
+    $('#main_menu_toggle').hide();
+  });
+  $('#filter_menu_toggle').on('click', function(event){
+    $('#filter_menu').removeClass('visible');
+    $('#main-overlay').hide();
+    $('#main_menu_toggle').fadeIn(1000);
+  })
 
 
   $('.grid').masonry({
