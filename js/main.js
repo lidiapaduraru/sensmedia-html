a848920e7bb05b0eb7d713c6e1d24d8757b799cb
Lidia Paduraru <lidia.paduraru@gmail.com>
2018-03-23 14:03:32 +0200

    Page toggle links with scroll

--- js/main.js
+++ js/main.js
@@ -1,74 +1,5 @@
 jQuery(document).ready(function(){
 
-
-  var sidebarTrigger = $('#main_menu_toggle');
-  sidebarTrigger.on('click', function(event){
-    event.preventDefault();
-    if(sidebarTrigger.hasClass('nav-is-visible')){
-      $('#main_sidebar_menu').removeClass('visible');
-      $('#main-overlay').hide();
-    }else{
-      $('#main_sidebar_menu').addClass('visible');
-      $('#main-overlay').show();
-    }
-    $([sidebarTrigger]).toggleClass('nav-is-visible');
-  });
-
-  $('#filter_button').on('click', function(event){
-    event.preventDefault();
-    $('#filter_menu').addClass('visible');
-    $('#main-overlay').show();
-    $('#main_menu_toggle').hide();
-  });
-  $('#filter_menu_toggle').on('click', function(event){
-    $('#filter_menu').removeClass('visible');
-    $('#main-overlay').hide();
-    $('#main_menu_toggle').fadeIn(1000);
-  })
-
-  $('.cos_button').on('click', function(event){
-    event.preventDefault();
-    $('#cos_menu').addClass('visible');
-    $('#main-overlay').show();
-    $('#main_menu_toggle').hide();
-  });
-  $('#cos_menu_toggle').on('click', function(event){
-    $('#cos_menu').removeClass('visible');
-    $('#main-overlay').hide();
-    $('#main_menu_toggle').fadeIn(1000);
-  })
-
-  $('#auth_button').on('click', function(event){
-    event.preventDefault();
-    $('#auth_menu').addClass('visible');
-    $('#auth_register').show();
-    $('#auth_reset').hide();
-    $('#main-overlay').show();
-    $('#main_menu_toggle').hide();
-  });
-  $('#auth_menu_toggle').on('click', function(event){
-    $('#auth_menu').removeClass('visible');
-    $('#main-overlay').hide();
-    $('#main_menu_toggle').fadeIn(1000);
-  });
-  $('#forgot_password').on('click', function(event){
-    event.preventDefault();
-    $('#auth_register').hide();
-    $('#auth_reset').show();
-  });
-
-  $('#auth_button').on('click', function(event){
-    event.preventDefault();
-    $('#auth_menu').addClass('visible');
-    $('#main-overlay').show();
-    $('#main_menu_toggle').hide();
-  });
-  $('#auth_menu_toggle').on('click', function(event){
-    $('#auth_menu').removeClass('visible');
-    $('#main-overlay').hide();
-    $('#main_menu_toggle').fadeIn(1000);
-  })
-
   $('.btn.minus').on('click', function(event){
     var s = parseInt($(this).next().val()) - 1
     if(s < 0){
@@ -100,6 +31,16 @@ jQuery(document).ready(function(){
     return $(document).find('.collapse.in').collapse('hide');
   });
 
+  $(".go_toggle").on('click', function(event){
+    event.preventDefault();
+    $('.go_toggle').removeClass('active');
+    $(this).addClass('active');
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top +150
+    }, 500);
+    return false;
+  });
+
   $('.carousel').carousel();
 
 
@@ -116,11 +57,6 @@ jQuery(document).ready(function(){
     }, 800);
     return false;
   });
-
-
-
-
-
 
 
 
