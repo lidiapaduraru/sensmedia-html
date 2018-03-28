2300a24bc694fa024f17169a59c36ae9c24a29b9
Lidia Paduraru <lidia.paduraru@gmail.com>
2018-03-28 12:32:39 +0300

    Slider add shadow text for next item

--- js/vendor/bootstrap-touch-slider.js
+++ js/vendor/bootstrap-touch-slider.js
@@ -12,25 +12,16 @@
         var carousel = $( "#bootstrap-touch-slider" );
         return this.each( function ( ) {
 
-            function doAnimations( animatingInElems, animatingOutElems ) {
+            function doAnimations( animatingElems ) {
                 //Cache the animationend event in a variable
                 var animEndEv = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-                animatingInElems.each( function ( ) {
+                animatingElems.each( function ( ) {
                     var $this = $( this ),
                         $animationType = $this.data( 'animation' );
                     $this.addClass( $animationType ).one( animEndEv, function ( ) {
                         $this.removeClass( $animationType );
                     } );
                 } );
-                if(animatingOutElems != null) {
-                    animatingOutElems.each( function ( ) {
-                        var $this = $( this ),
-                            $animationType = "animation fadeOutRight";
-                        $this.addClass( $animationType ).one( animEndEv, function ( ) {
-                            $this.removeClass( $animationType );
-                        } );
-                    } );
-                }
             }
 
             //Variables on page load
@@ -38,14 +29,32 @@
             //Initialize carousel
             carousel.carousel( );
             //Animate captions in first slide on page load
-            doAnimations( $firstAnimatingElems, null );
+            doAnimations( $firstAnimatingElems );
+            carousel.find('.carousel-inner').children(':nth-child(2)').delay(1000).queue(function(){
+                $(this).addClass('shaded');
+                $(this).clearQueue();
+            });
             //Other slides to be animated on carousel slide event
             carousel.on( 'slide.bs.carousel', function ( e ) {
                 var $animatingInElems = $( e.relatedTarget ).find( "[data-animation ^= 'animated']" );
-                var $oldItem = $( e.relatedTarget ).next();
-                var $animatingOutElems = $oldItem.find( "[data-animation ^= 'animated']" );
-                doAnimations( $animatingInElems, null );
+                doAnimations( $animatingInElems );
+                $( '#bootstrap-touch-slider .item' ).removeClass('shaded');
             } );
+
+            var csize = carousel.find('.carousel-inner').children().size();
+            carousel.on( 'slid.bs.carousel', function ( e ) {
+                if(csize > 1){
+                    if($( e.relatedTarget ).index() + 1 == csize){
+                        var $nextElem = $( e.relatedTarget ).parent().children(':first-child');
+                    }else{
+                        var $nextElem = $( e.relatedTarget ).next();
+                    }
+                    $nextElem.delay(1000).queue(function(){
+                        $(this).addClass('shaded');
+                        $(this).clearQueue();
+                    });
+                }
+            });
 
         } );
     };
